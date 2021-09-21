082c381496602a370cead404177c27829e1830a9
samuel Odubamowo <wemsamuel@gmail.com>
2021-09-21 13:27:39 -0700

    added index to each i

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
 const emoji = ['😡', '❤️', '👌', '🙈', '😭', '🎶', '👀', '😇', '🤬', '🤯', '😶‍🌫️', '😵‍💫', '🤢', '👿', '💀', '👽', '👾', '🤖', '🎃', '🦾', '👀', '🧛🏿', '🧞', '🧚‍♂️', '🕺', '😡', '❤️', '👌', '🙈', '😭', '🎶', '👀', '😇', '🤬', '🤯', '😶‍🌫️', '😵‍💫', '🤢', '👿', '💀', '👽', '👾', '🤖', '🎃', '🦾', '👀', '🧛🏿', '🧞', '🧚‍♂️', '🕺']
 
+const emojiClk = document.querySelector('.grid-item');
 
 
 const shuffle = (array) => {
     let newLength = array.length, availableEmoji, currentEmoji
-
+    
     while (newLength) {
         currentEmoji = Math.trunc(Math.random() *  newLength--)
         availableEmoji = array[newLength];
         array[newLength] = array[currentEmoji]
         array[currentEmoji] = availableEmoji
     };
-
+    
     return array
 }
 
 
 const newGame = () => {
-shuffle(emoji)
-for (let i=0; i < emoji.length; i++) {
-    const newDiv = document.createElement('div');
-    const eachEmoji = document.createTextNode(emoji[i]);
-    newDiv.appendChild(eachEmoji)
-    newDiv.setAttribute('class', 'grid-item');
-    document.querySelector('.grid-container').appendChild(newDiv);
-    
-}
-}
-newGame()
-document.addEventListener('click', function() {
-    const click = document.querySelectorAll('grid-item').textContent
-    console.log(click);
+    shuffle(emoji)
+    for (let i=0; i < emoji.length; i++) {
+        const newDiv = document.createElement('div');
+        newDiv.textContent = emoji[i]
+        console.log(emoji.indexOf(emoji[i]))
+        newDiv.setAttribute('class', `item-${emoji.indexOf(emoji[i])}`);
+        newDiv.setAttribute('class', 'grid-item');
+        // document.querySelector('.grid-item').classList.add(`item-${emoji.indexOf(emoji[i])}`);
 
-})
+        document.querySelector('.grid-container').appendChild(newDiv);
+    }
+    
+    // emojiClk.addEventListener('click', function() {
+        //     // emojiClk
+        //     console.log('im here');
+        
+        // })
+    }   
+    newGame();
+    emojiClk.addEventListener('click', function() {
+        console.log('emojiClk');
+
+    });
