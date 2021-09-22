a9366d28cb0fd3168a0f298fb09a8edc5ef6c53a
samuel Odubamowo <wemsamuel@gmail.com>
2021-09-21 23:08:37 -0700

    add points & emoji click is now functional

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const emoji = ['😡', '❤️', '👌', '🙈', '😭', '🎶', '👀', '😇', '🤬', '🤯', '😶‍🌫️', '😵‍💫', '🤢', '👿', '💀', '👽', '👾', '🤖', '🎃', '🦾', '👀', '🧛🏿', '🧞', '🧚‍♂️', '🕺', '😡', '❤️', '👌', '🙈', '😭', '🎶', '👀', '😇', '🤬', '🤯', '😶‍🌫️', '😵‍💫', '🤢', '👿', '💀', '👽', '👾', '🤖', '🎃', '🦾', '👀', '🧛🏿', '🧞', '🧚‍♂️', '🕺']
+const emoji = ['❤️', '🙈', '😭', '🎶', '🤬', '🤯', '😶‍🌫️', '🤢', '👿', '💀', '👽', '👾', '🤖', '🎃', '🦾', '🧛🏿', '🧞', '🧚‍♂️', '🕺', '❤️', '🙈', '😭', '🎶', '👀', '🤬', '🤯', '😶‍🌫️', '🤢', '👿', '💀', '👽', '👾', '🤖', '🎃', '🦾', '👀', '🧛🏿', '🧞', '🧚‍♂️', '🕺']
 
-const emojiClk = document.querySelector('.grid-item');
+const pointsEl = document.getElementById('pointsEl')
 
+let points = 0;
 
 const shuffle = (array) => {
-    let newLength = array.length, availableEmoji, currentEmoji
+    let total = array.length
+    let availableEmojis, currentEmoji
     
-    while (newLength) {
-        currentEmoji = Math.trunc(Math.random() *  newLength--)
-        availableEmoji = array[newLength];
-        array[newLength] = array[currentEmoji]
-        array[currentEmoji] = availableEmoji
+    while (total) {
+        currentEmoji = Math.trunc(Math.random() *  total--)
+        availableEmojis = array[total];
+        array[total] = array[currentEmoji]
+        array[currentEmoji] = availableEmojis
     };
     
     return array
@@ -22,22 +24,41 @@ const newGame = () => {
     for (let i=0; i < emoji.length; i++) {
         const newDiv = document.createElement('div');
         newDiv.textContent = emoji[i]
-        console.log(emoji.indexOf(emoji[i]))
-        newDiv.setAttribute('class', `item-${emoji.indexOf(emoji[i])}`);
-        newDiv.setAttribute('class', 'grid-item');
+        // console.log(emoji.indexOf(emoji[i]))
+        // console.log(newDiv.setAttribute('class', `item-${emoji.indexOf(emoji[i])}`));
         // document.querySelector('.grid-item').classList.add(`item-${emoji.indexOf(emoji[i])}`);
-
+        newDiv.setAttribute('class', 'grid-item');
         document.querySelector('.grid-container').appendChild(newDiv);
-    }
-    
-    // emojiClk.addEventListener('click', function() {
-        //     // emojiClk
-        //     console.log('im here');
-        
-        // })
-    }   
-    newGame();
-    emojiClk.addEventListener('click', function() {
-        console.log('emojiClk');
+        newDiv.addEventListener('click', function() {
+            const current = document.getElementsByClassName('current')
+            newDiv.classList.add('current')
 
-    });
+
+            if(current.length === 2) {
+
+                if(current[0].textContent === current[1].textContent) {
+                    current[0].classList.add('match')
+                    current[1].classList.add('match')
+                    current[1].classList.remove('current')
+                    current[0].classList.remove('current')
+                    points += 7;
+                    pointsEl.textContent = points
+                    console.log(points)
+                }else{
+                    console.log('cover me please!');
+                    current[1].classList.remove('current')
+                    current[0].classList.remove('current')
+                }
+            }else if(current.length >= 3){
+                newDiv.classList.remove('current')
+            }
+        })
+
+        
+    }
+    const emojiClk = document.querySelectorAll('.grid-item');
+    // console.log(emojiClk);
+    
+}   
+newGame();
+
